f57847ae049d138127fc386184b0bc24c0fd1a22
Filip Carevic <filip.carevic11@gmail.com>
2020-03-28 18:59:10 +0100

    ispravke nakon FR faze

---
+++ PROTOTIP/js/ankete.js
@@ -0,0 +1,14 @@
+function posalji()
+{
+if(document.getElementById("naziv")).value==null) alert("Uneti naziv ankete");
+				if(document.getElementById("lokalni")).checked) alert(document.getElementById("cities").value);
+
+
+				window.location="nemaanketa.html";
+return true;
+
+}
+function send(){
+alert('usao');
+
+}
